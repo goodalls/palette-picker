88e5d97dc28514983b3a5dfeee8a84083270eb03
Jeff Goodall <goodallhj@gmail.com>
2018-03-12 13:58:51 -0600

    Initial Commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+});
