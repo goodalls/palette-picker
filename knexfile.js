cb29f5cb105b3d7bef48aff3905dc40e3b05e7ed
Jeff Goodall <goodallhj@gmail.com>
2018-03-16 10:46:42 -0600

    adding production environment for heroku

--- knexfile.js
+++ knexfile.js
@@ -21,5 +21,14 @@ module.exports = {
       directory: './test/db/seeds/dev'
     },
     useNullAsDefault: true
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
   }
 };
