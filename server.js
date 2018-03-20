69378d0fa806b4dff9d697f10c6fa1e99b7c820e
Jeff Goodall <goodallhj@gmail.com>
2018-03-20 10:15:12 -0600

    adding https re-direct

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-
+//
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
-
+//middlewear setup for body parser and port and setting the title to 'palette picker'
 app.use(bodyParser.json());
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Palette Picker';
 
 app.use(express.static('public'));
 
+// for some reason this code was breaking my tests. but the docs made it seem like I did it correctly.
+
 // app.use((request, respond) => {
 //   respond.status(404).send('Sorry, that is not found.');
 // });
+
+app.get('*', (request, response)=> {
+  response.redirect('https://' + request.headers.host + request.url);
+});
 
 app.get('/api/v1/palettes', (request, response) => {
   database('palettes')
