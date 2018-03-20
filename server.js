90e64413e9e385b2b4dacc5fec446f81a35e8d1c
Jeff Goodall <goodallhj@gmail.com>
2018-03-20 10:58:30 -0600

    clean up imports

--- server.js
+++ server.js
@@ -2,11 +2,6 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-
-const https = require('https');
-const http = require('http');
-
-
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
